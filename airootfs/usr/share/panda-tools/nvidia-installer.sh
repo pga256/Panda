2e64fa888fba18db6543276f433ddc9e6b729a2f
pga256 <peter.g.anderson@live.co.uk>
2023-06-10 16:23:42 +0100

    Added Nvidia installer script and icons.

---
+++ airootfs/usr/share/panda-tools/nvidia-installer.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+
+pacman -Sy nvidia-dkms nvidia-utils lib32-nvidia-utils nvidia-settings /
+vulkan-icd-loader lib32-vulkan-icd-loader
+
+sync
+
+read -s -n 1 -p "Nvidia drivers have been installed. ** Reboot recommended. ** Press a key to exit . . ." 
+exit
