4b5373e38b4afcca315521992a14e80662947f42
pga256 <peter.g.anderson@live.co.uk>
2023-06-12 00:22:45 +0100

    Nvidia installer script now detects legacy cards requiring the 470xx series of drivers.

--- airootfs/usr/share/panda-tools/nvidia-installer.sh
+++ airootfs/usr/share/panda-tools/nvidia-installer.sh
@@ -1,9 +1,31 @@
 #! /bin/bash
 
-pacman -Sy nvidia-dkms nvidia-utils lib32-nvidia-utils nvidia-settings \
-vulkan-icd-loader lib32-vulkan-icd-loader --noconfirm
-
+$CARD=$(lspci |grep VGA)
+NV=0
+NVLOADED=""
+for line in $(cat ./Nvidia_470.list); do
+    if [[ $CARD = *$line* ]]
+    then
+        echo Legacy Nvidia card Detected.  Installing Nvidia 470 Driver components.
+        NV=1
+    fi    
+done
+if [[ $NV=1 ]]
+    then
+        echo Legacy Nvidia card Detected.  Installing Nvidia 470 Driver components.
+        pacman -Sy nvidia-470xx-dkms nvidia-470xx-utils lib32-nvidia-470xx-utils nvidia-470xx-settings \
+      vulkan-icd-loader lib32-vulkan-icd-loader --noconfirm
+    else
+        echo Nvidia card Detected.  Installing Nvidia Driver components.
+        pacman -Sy nvidia-dkms nvidia-utils lib32-nvidia-utils nvidia-settings \
+        vulkan-icd-loader lib32-vulkan-icd-loader --noconfirm
+fi
 sync
-
-read -s -n 1 -p "Nvidia drivers have been installed. ** Reboot recommended. ** Press a key to exit . . ." 
+NVLOADED=$(lspci -k | grep -A 2 -E "(VGA|3D)" |grep "Kernel driver in use: nvidia")
+if  [[ $NVLOADED ]]
+    then    
+        read -s -n 1 -p "SUCCESS!  Nvidia drivers have been installed. ** Reboot recommended. ** Press a key to exit . . ."
+    else
+        read -s -n 1 -p "Nvidia drivers have not been installed. Press a key to exit . . ."
+fi
 exit
