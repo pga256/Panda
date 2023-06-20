47bec1d49b0520ccedb3c1ffd8706c16cf734105
pga256 <peter.g.anderson@live.co.uk>
2023-06-20 09:37:27 +0100

    Removed some superfluous filesfron build folder.

--- fullbuild.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-cp ./full.x86_64 ./packages.x86_64
-cp ./profilefull.sh ./profiledef.sh
-./mkpandaiso -v -w ../tmp/game-v2-offline -o ./out ./
-sync
-rm ./packages.x86_64
-rm ./profiledef.sh
-exit
-
