8c939db66b5e7a6dc78c474f9dea71fa30201f46
pga256 <peter.g.anderson@live.co.uk>
2023-06-21 21:57:04 +0100

    Removed redundant calamares modules. chaotic-aur disabled by default. Added .gitignore

--- minbuild.sh
+++ minbuild.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 cp ./minimal.x86_64 ./packages.x86_64
 cp ./profileminimal.sh ./profiledef.sh
-./mkpandaiso -v -w /tmp/game-v2-offline -o ./out ./
+./mkpandaiso -v -w /tmp/panda -o ./out ./
 sync
 rm ./packages.x86_64
 rm ./profiledef.sh
