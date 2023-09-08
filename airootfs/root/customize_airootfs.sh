640170dceb52806ef2ffcb2f6363aeb38250bcc8
pga256 <panda.linux@outlook.com>
2023-09-08 13:21:08 +0100

    Removed SearX search engine from firefox. Added new pga256 public key.

--- airootfs/root/customize_airootfs.sh
+++ airootfs/root/customize_airootfs.sh
@@ -5,9 +5,11 @@ pacman-key --populate archlinux chaotic
 pacman-key --recv-key FBA220DFC880C036 --keyserver keyserver.ubuntu.com
 pacman-key --recv-key 349BC7808577C592 --keyserver keyserver.ubuntu.com
 pacman-key --recv-key EE4780313838BB4D --keyserver keyserver.ubuntu.com
+pacman-key --recv-key 29CD02430D2ABA44 --keyserver keyserver.ubuntu.com
 pacman-key --lsign-key EE4780313838BB4D
 pacman-key --lsign-key 349BC7808577C592
 pacman-key --lsign-key FBA220DFC880C036
+pacman-key --lsign-key 29CD02430D2ABA44
 pacman -Syy
 
 systemctl enable NetworkManager.service
